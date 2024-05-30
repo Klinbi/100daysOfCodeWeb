c17136715f72d0b803af10b25800c22432fe8007
Vitezslav Linke <vlinke88@gmail.com>
2024-05-30 22:56:01 +0200

    Day 30, 31, 32, 33

--- javascript-introduction/app.js
+++ javascript-introduction/app.js
@@ -1,22 +1,3 @@
-const nameInputElement = document.getElementById('product-name');
-const remainingCharsElement = document.getElementById('remaining-chars');
+console.dir(document)
 
-const maxAllowedChars = nameInputElement.maxLength;
-
-function updateRemainingChars() {
-    const enteredValue = nameInputElement.value;
-
-    const remainingCharacters = maxAllowedChars - enteredValue.length;
-
-    remainingCharsElement.textContent = remainingCharacters;
-
-    if (remainingCharacters <= 10) {
-        nameInputElement.classList.add('warning');
-        remainingCharsElement.classList.add('warning');
-    } else {
-        nameInputElement.classList.remove('warning');
-        remainingCharsElement.classList.remove('warning');
-    }
-}
-
-nameInputElement.addEventListener('input', updateRemainingChars);
+document.querySelector("a").href = "https://facebook.com";
